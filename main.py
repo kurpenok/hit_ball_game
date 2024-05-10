bf3efb797d3b3101517a944733fc32ba7d6195c3
Kurpenov Kuat <kurpenov2003@yandex.ru>
2024-05-10 21:17:07 +0600

    Added source code

---
+++ main.py
@@ -0,0 +1,90 @@
+import random
+import time
+from tkinter import Canvas, Tk
+
+
+class Paddle:
+    def __init__(self, canvas: Canvas, color) -> None:
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
+        self.canvas.move(self.id, 200, 300)
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+        self.canvas.bind_all("<KeyPress-Left>", self.turn_left)
+        self.canvas.bind_all("<KeyPress-Right>", self.turn_right)
+
+    def draw(self) -> None:
+        self.canvas.move(self.id, self.x, 0)
+        pos = self.canvas.coords(self.id)
+        if pos[0] <= 0:
+            self.x = 0
+        elif pos[2] >= self.canvas_width:
+            self.x = 0
+
+    def turn_left(self, evt) -> None:
+        self.x = -2
+
+    def turn_right(self, evt) -> None:
+        self.x = 2
+
+
+class Ball:
+    def __init__(self, canvas: Canvas, paddle: Paddle, color) -> None:
+        self.canvas = canvas
+        self.paddle = paddle
+        self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
+        self.canvas.move(self.id, 245, 100)
+        starts = [-3, -2, -1, 1, 2, 3]
+        random.shuffle(starts)
+        self.x = starts[0]
+        self.y = -3
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
+        self.hit_bottom = False
+
+    def hit_paddle(self, pos) -> bool:
+        paddle_pos = self.canvas.coords(self.paddle.id)
+        if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
+            if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+                return True
+        return False
+
+    def draw(self) -> None:
+        self.canvas.move(self.id, self.x, self.y)
+        pos = self.canvas.coords(self.id)
+        if pos[1] <= 0:
+            self.y = 3
+        if pos[3] >= self.canvas_height:
+            self.hit_bottom = True
+        if self.hit_paddle(pos):
+            self.y = -3
+        if pos[0] <= 0:
+            self.x = 3
+        if pos[2] >= self.canvas_width:
+            self.x = -3
+
+
+def close_window(event):
+    tk.destroy()
+
+
+if __name__ == "__main__":
+    tk = Tk()
+    tk.title("Hit Ball!")
+    tk.bind("<q>", close_window)
+    tk.resizable(False, False)
+    tk.wm_attributes("-topmost", 1)
+    canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
+    canvas.pack()
+    tk.update()
+
+    paddle = Paddle(canvas, "blue")
+    ball = Ball(canvas, paddle, "red")
+
+    while True:
+        if ball.hit_bottom == False:
+            ball.draw()
+            paddle.draw()
+        tk.update_idletasks()
+        tk.update()
+        time.sleep(0.01)
